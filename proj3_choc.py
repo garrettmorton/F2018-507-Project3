20c82553e42b5e45386de9011319ea8dd009a200
Garrett Morton <gemorton@gmail.com>
2018-11-20 13:10:44 -0500

    Update

--- proj3_choc.py
+++ proj3_choc.py
@@ -2,6 +2,10 @@ import sqlite3
 import csv
 import json
 
+import sys
+import codecs
+sys.stdout = codecs.getwriter('utf-8')(sys.stdout.buffer)
+
 # proj3_choc.py
 # You can change anything in this file you want as long as you pass the tests
 # and meet the project requirements! You will need to implement several new
@@ -11,6 +15,84 @@ import json
 DBNAME = 'choc.db'
 BARSCSV = 'flavors_of_cacao_cleaned.csv'
 COUNTRIESJSON = 'countries.json'
+
+
+
+conn = sqlite3.connect(DBNAME)
+cur = conn.cursor()
+
+## create Countries table
+statement = '''
+CREATE TABLE Countries (
+    Id INTEGER PRIMARY KEY AUTOINCREMENT,
+    Alpha2 TEXT,
+    Alpha3 TEXT,
+    EnglishName TEXT,
+    Region TEXT,
+    Subregion TEXT,
+    Population INTEGER,
+    Area REAL
+)
+'''
+try:
+    cur.execute(statement)
+except:
+    pass
+
+## populate Countries Table
+f = open(COUNTRIESJSON, 'r')
+f_text = f.read()
+countries_json = json.loads(f_text)
+f.close()
+fields = ['alpha2Code', 'alpha3Code', 'name', 'region', 'subregion', 'population', 'area']
+
+for item in countries_json:
+    item_values = []
+    for field in fields:
+        if field in item.keys():
+            item_values.append(item[field])
+        else:
+            item_values.append('')
+    statement = '''
+    INSERT INTO Countries (Alpha2, Alpha3, EnglishName, Region, Subregion, Population, Area)
+    VALUES (?,?,?,?,?,?,?)
+    '''
+    cur.execute(statement, item_values)
+    
+
+# ## create Bars table
+# statement = '''
+# CREATE TABLE Bars (
+#     Id INTEGER PRIMARY KEY AUTOINCREMENT,
+#     Company TEXT,
+#     SpecificBeanBarName TEXT,
+#     REF TEXT,
+#     ReviewDate TEXT,
+#     CocoaPercent REAL,
+#     CompanyLocationId INTEGER,
+#     Rating REAL,
+#     BeanType TEXT,
+#     BroadBeanOriginId INTEGER
+#     )
+# '''
+# cur.execute(statement)
+
+# ## populate Bars table
+# with open(BARSCSV) as csvDataFile:
+#     csvReader = csv.reader(csvDataFile)
+#     for row in csvReader:
+#         items = []
+#         for i in range()
+
+#         statement = '''
+#         INSERT INTO Bars (Company, SpecificBeanBarName, REF, ReviewDate, CocoaPercent, CompanyLocationId, Rating, BeanType, BroadBeanOriginId)
+#         VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+#         '''
+#         cur.execute(statement, items)
+#         print("added!")
+#     csvDataFile.close()
+
+conn.close()
 
 
 # Part 2: Implement logic to process user commands
@@ -34,5 +116,5 @@ def interactive_prompt():
             continue
 
 # Make sure nothing runs or prints out when this file is run as a module
-if __name__=="__main__":
-    interactive_prompt()
+# if __name__=="__main__":
+#     interactive_prompt()
